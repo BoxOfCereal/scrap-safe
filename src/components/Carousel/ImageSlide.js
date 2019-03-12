76ea4e099fab36ad84616eac993de6faa9b558d0
Noah <noah.stoeckel@gmail.com>
2019-03-12 00:33:52 -0400

    Adds Overlay to Image Slides

--- src/components/Carousel/ImageSlide.js
+++ src/components/Carousel/ImageSlide.js
@@ -7,7 +7,17 @@ const ImageSlide = ({ url, alt, classnames }) => {
       className={`img-slide ${classnames}`}
       style={{ backgroundImage: `url(${url})` }}
       alt={alt}
-    />
+    >
+      <div className="img-cover">
+        <div className="img-text-wrapper">
+          <p className="img-title">WASTE MANAGEMENT</p>
+          <p className="img-text">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio unde
+            minus ullam illum iusto.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
